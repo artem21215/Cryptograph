3ae8a1cbe87630ebfd33358435c0bbc7401705e7
artem <13_22_54@mail.ru>
2022-10-15 17:53:47 +0700

    Add dirty implement for crypthoghaph files

---
+++ modulArithmetics.h
@@ -0,0 +1,24 @@
+//
+// Created by artem on 06.10.2022.
+//
+
+#ifndef THEORYPROGRAMMINGLANGUAGE_MODULARITHMETICS_H
+#define THEORYPROGRAMMINGLANGUAGE_MODULARITHMETICS_H
+#include <vector>
+
+using ll = long long;
+struct GcdReturn{
+    ll x{};
+    ll y{};
+    ll g{};
+};
+
+ll fastPow(ll a, ll step, ll mod);
+
+GcdReturn gcd(ll a, ll b);
+
+int chooseRandomQ();
+
+int chooseRandomG(int mod, int q);
+
+#endif //THEORYPROGRAMMINGLANGUAGE_MODULARITHMETICS_H
